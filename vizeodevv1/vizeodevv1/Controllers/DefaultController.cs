4b6eed01f51d9e450897577eddbb3d0dafb475c0
batuhanglryz <60665256+batuhanglryz@users.noreply.github.com>
2020-12-05 19:45:00 +0300

    vizeodevi1

---
+++ vizeodevv1/vizeodevv1/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vizeodevv1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Formula()
+        {
+            ViewBag.message = "formula sayfası";
+            return View();
+        }
+        public ActionResult Kazananlar()
+        {
+            ViewBag.message = "SON 5 YARIŞ KAZANANLAR";
+            return View();
+        }
+    }
+
+}
---
+++ vizeodevv1/vizeodevv1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vizeodevv1.MvcApplication" Language="C#" %>
